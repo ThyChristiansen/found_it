4f2b0d02c6d59e653cd54f765e39697436d8bcd9
ThyChristiansen <thyvu0411@gmail.com>
2020-05-30 13:08:35 -0500

    add room id for link qr

--- src/components/DownloadQRCode/DownloadQRCode.js
+++ src/components/DownloadQRCode/DownloadQRCode.js
@@ -11,7 +11,7 @@ class DownloadQRCode extends Component {
             .replace("image/png", "image/octet-stream");
         let downloadLink = document.createElement("a");
         downloadLink.href = pngUrl;
-        downloadLink.download = `QR_code_box_number_${this.props.box.qr_code}.png`;
+        downloadLink.download = `QR_code_roomId${this.props.box.room_id}_boxId${this.props.box.qr_code}.png`;
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
@@ -21,12 +21,14 @@ class DownloadQRCode extends Component {
             <div>
                 <QRCode
                     id={this.props.box.qr_code}
-                    value = {`https://f0undit.herokuapp.com/#/box-detail/${this.props.box.qr_code}`}
+                    value={`https://f0undit.herokuapp.com/#/box-detail/${this.props.box.room_id}/${this.props.box.qr_code}`}
                     size={290}
                     level={"H"}
                     includeMargin={true}
                 /><br />
                 <button onClick={this.downloadQR}> Download QR </button>
+                {/* <h1>{JSON.stringify(this.props.box.room_id)}</h1> */}
+
             </div>
 
         )
