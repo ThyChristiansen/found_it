7526b78d610593cadc47b4ba51dcb156b717503b
ThyChristiansen <thyvu0411@gmail.com>
2020-06-05 17:03:47 -0500

    styling for box list and room name

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { withRouter } from "react-router";
 
 
 class Box extends Component {
+
+    handleClick = () => {
+        console.log('clicked');
+        this.props.history.push(`/box-detail/${this.props.box.room_id}/${this.props.box.id}`)
+    }
 
     render() {
         // box's name condition for each room 
         let boxName;
         if (this.props.box.room_name === 'Storage') {
-            boxName = <p className="box_name">A{this.props.box.box_name}</p>
+            boxName = <p className="box_name_in_box_list">A{this.props.box.box_name}</p>
         } if (this.props.box.room_name === 'Basement') {
-            boxName = <p className="box_name">B{this.props.box.box_name}</p>
+            boxName = <p className="box_name_in_box_list">B{this.props.box.box_name}</p>
         }
         if (this.props.box.room_name === 'Garage') {
-            boxName = <p className="box_name">C{this.props.box.box_name}</p>
+            boxName = <p className="box_name_in_box_list">C{this.props.box.box_name}</p>
         }
         if (this.props.box.room_name === 'Livingroom') {
             boxName =
-                <p className="box_name">D{this.props.box.box_name}</p>
+                <p className="box_name_in_box_list">D{this.props.box.box_name}</p>
         }
         if (this.props.box.room_name === 'Bedroom') {
             boxName =
-                <p className="box_name">E{this.props.box.box_name}</p>
+                <p className="box_name_in_box_list">E{this.props.box.box_name}</p>
         }
         if (this.props.box.room_name === 'Kitchen') {
             boxName =
-                <p className="box_name">F{this.props.box.box_name}</p>
+                <p className="box_name_in_box_list">F{this.props.box.box_name}</p>
         }
 
         //Use if condition to change the box visual if they was opened or not yet
@@ -53,14 +59,12 @@ class Box extends Component {
             <div>
                 {/* <p>{JSON.stringify(this.props.box.status)}</p> */}
                 {/* Assigning the link to box icon to navigating user to box's detail page  */}
-                <Link to={`/box-detail/${this.props.box.room_id}/${this.props.box.id}`}>
+                <div className= "item_box_inbox_list"onClick={this.handleClick}>
                     {boxStatus}
-                </Link>
-                <div className = "box_name_in_box_list">{boxName}</div>
-
-               
+                    {boxName}
+                </div>
             </div>
         )
     }
 }
-export default connect()(Box);
+export default withRouter(connect()(Box));
