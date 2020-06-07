7f7fb704e9c4399d6647e8bda3f650cb80fe67e5
ThyChristiansen <thyvu0411@gmail.com>
2020-06-06 23:47:20 -0500

    fixing alert for sign up page

--- src/redux/reducers/loginModeReducer.js
+++ src/redux/reducers/loginModeReducer.js
@@ -1,27 +1,11 @@
-import Swal from 'sweetalert2';
 
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'bottom-end',
-  showConfirmButton: false,
-  timer: 8000,
-  // timerProgressBar: true,
-  onOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-})
-Toast.fire({
-  icon: 'success',
-  title: "You just succeeded in createing a new account. Now, let's start!!"
-})
 
 const loginMode = (state = 'login', action) => {
     switch (action.type) {
       case 'SET_TO_LOGIN_MODE':
         return 'login';
       case 'SET_TO_REGISTER_MODE':
-        return Toast;
+        return 'register';
       default:
         return state;
     }
