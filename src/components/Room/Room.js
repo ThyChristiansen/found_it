e46b0c28ea9b98a6dc3e0c9f0fa6ab263b0c3da6
ThyChristiansen <thyvu0411@gmail.com>
2020-06-07 19:01:23 -0500

    clickable for room name

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from "react-router";
 import './RoomList.css'
-
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class RoomList extends Component {
     handleOnClick = () => {
         console.log('room clicked!')
+        this.props.history.push(`/box-list/${this.props.roomData.id}`)
+
     }
 
     render() {
         return (
-            <div>
+            <div onClick={this.handleOnClick}>
                 {/* <h1>{JSON.stringify(this.props.reduxState.rooms)}</h1> */}
                 {/* Set Link for each room icon so that the user can click on the box icon and navigate to the detail inside room */}
-                <Link to={`/box-list/${this.props.roomData.id}`}>
-                    <img src="images/room_icon.png"
-                        alt="room_icon"
-                        width="75px"
-                        height="75px"
-                        onClick={this.handleOnClick}
-                    ></img>
-                </Link>
+                {/* <Link to={`/box-list/${this.props.roomData.id}`}> */}
+                <img src="images/room_icon.png"
+                    alt="room_icon"
+                    width="75px"
+                    height="75px"
+                ></img>
                 {/* Room's mame */}
                 <p className="room_name_in_list_room">{this.props.roomData.room_name}</p>
+                {/* </Link> */}
             </div >
         )
     }
 }
 const putReduxStateToProps = (reduxState) => ({ reduxState });
-export default connect(putReduxStateToProps)(RoomList);
+export default withRouter(connect(putReduxStateToProps)(RoomList));
