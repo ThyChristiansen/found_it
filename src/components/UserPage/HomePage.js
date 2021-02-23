dc58773690dfb357b4789adfdf16bd65231a9e67
Thy Christiansen <thyvu0411@gmail.com>
2021-02-23 16:09:01 -0600

    clean up code

--- src/components/UserPage/HomePage.js
+++ src/components/UserPage/HomePage.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Room from '../Room/Room';
-import './UserPage.css'
+import { Paper, Slide } from "@material-ui/core";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Room from "../Room/Room";
+import "./UserPage.css";
 
 class HomePage extends Component {
-    componentDidMount() {
-        //Send this action to Saga to show the house's name 
-        this.props.dispatch({
-            type: "FETCH_HOUSE_NAME",
-            payload: {
-                userId: this.props.userId
-            }
-        });
-    }
-git
-    render() {
-        return (
-            <div className = "room_list">
-                {/* Mapping through the houseName reducer and display the house's name */}
-                <h1 className="house_name">{this.props.reduxState.houseName.map((event) => event.house_name)}</h1>
+  componentDidMount() {
+    //Send this action to Saga to show the house's name
+    this.props.dispatch({
+      type: "FETCH_HOUSE_NAME",
+      payload: {
+        userId: this.props.userId,
+      },
+    });
+  }
+  git;
+  render() {
+    return (
+      <div className="room_list">
+        {/* Mapping through the houseName reducer and display the house's name */}
+        <h1 className="house_name">
+          {this.props.reduxState.houseName.map((event) => event.house_name)}
+        </h1>
 
-                {/* Mapping through the rooms reducer and display the list of room name */}
-                {this.props.reduxState.rooms.map((room) => {
-                    return (
-                        <div key={room.id} className="room">
-                            {/* I keep each room inside of the Room component */}
-                            <Room
-                                roomData={room}
-                            />
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
+        {/* Mapping through the rooms reducer and display the list of room name */}
+        {this.props.reduxState.rooms.map((room) => {
+          return (
+            <Slide direction="up" in={"true"} mountOnEnter unmountOnExit>
+              <div key={room.id} className="room">
+                {/* I keep each room inside of the Room component */}
+                <Room roomData={room} />
+              </div>
+            </Slide>
+          );
+        })}
+      </div>
+    );
+  }
 }
 //set the reduxState inside props and putReduxStateToProps variable inside the conneect
 //so that I can get get data from the reducers
